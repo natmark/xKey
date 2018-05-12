120c55c94bf9cea2903ea936988045b14a0821ed
natmark <natmark0918@gmail.com>
2018-05-12 16:08:45 +0900

    :new: Add XKey.swift

---
+++ XKey/XKey.swift
@@ -0,0 +1,13 @@
+//
+//  XKey.swift
+//  XKey
+//
+//  Created by AtsuyaSato on 2018/05/12.
+//  Copyright © 2018年 Atsuya Sato. All rights reserved.
+//
+
+import Foundation
+
+public class XKey {
+
+}
