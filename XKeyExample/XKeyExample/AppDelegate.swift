d85e485a0b612885ced9ce02859abb34a47688da
natmark <natmark0918@gmail.com>
2018-05-12 16:49:41 +0900

    :new: Add Example project

---
+++ XKeyExample/XKeyExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  XKeyExample
+//
+//  Created by AtsuyaSato on 2018/05/12.
+//  Copyright © 2018年 Atsuya Sato. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ XKeyExample/XKeyExample/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  XKeyExample
+//
+//  Created by AtsuyaSato on 2018/05/12.
+//  Copyright © 2018年 Atsuya Sato. All rights reserved.
+//
+
+import Cocoa
+import XKey
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
