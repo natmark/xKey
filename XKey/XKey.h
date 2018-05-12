04e8ba59c0cde0eb35a4e5cd430a68367ac29790
natmark <natmark0918@gmail.com>
2018-05-12 16:06:37 +0900

    :new: Add project

---
+++ XKey/XKey.h
@@ -0,0 +1,19 @@
+//
+//  XKey.h
+//  XKey
+//
+//  Created by AtsuyaSato on 2018/05/12.
+//  Copyright © 2018年 Atsuya Sato. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for XKey.
+FOUNDATION_EXPORT double XKeyVersionNumber;
+
+//! Project version string for XKey.
+FOUNDATION_EXPORT const unsigned char XKeyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XKey/PublicHeader.h>
+
+
